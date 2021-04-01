5686370f6ace134ef77ea91beb5367736e5cfe26
SeA75 <andrea.turolla@gmail.com>
2021-04-01 23:34:16 +0200

    add some file alter a long period :(

---
+++ BookProvider/RestRequestManager.cs
@@ -0,0 +1,39 @@
+﻿using RestSharp;
+using System;
+
+namespace LibraryAtHomeProvider
+{
+    public class RestRequestManager : IRestRequestManager
+    {
+        private readonly IRestClient _client;
+
+        public RestRequestManager(RestClient client)
+        {
+            if(client == null)
+            {
+                throw new ArgumentNullException(nameof(client), "client cannot be null");
+            } 
+            
+            _client = client;
+        }
+
+        public IRestResponseManager ExecuteGet()
+        {
+            var res = _client.Execute(new RestRequest(Method.GET));
+
+            if(res != null)
+            {
+                IRestResponseManager retval = new ResponseManager(res);
+
+                return retval;
+            }
+
+            return new ResponseManager();
+        }
+
+        public void SetBaseUri(Uri url)
+        {
+            _client.BaseUrl = url;
+        }
+    }
+}
---
+++ LibAtHomeWebApp/Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibAtHomeWebApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public string Message { get; set; }
+    }
+}
